8f69bfb8712e4bc83a7ef095a4301f34de7d77d7
Ben Schroeter <bschroeter1@setonix-04.head.cm.setonix.pawsey.org.au>
2025-01-09 12:38:18 +0800

    Updates from setonix

---
+++ hpcpy/status.py
@@ -0,0 +1,9 @@
+"""Class to manage the status of a job."""
+
+class Status:
+
+    def __init__(self, short, long, description, generic=None):
+        self.short = short
+        self.long = long
+        self.description = description
+        self.generic = generic
